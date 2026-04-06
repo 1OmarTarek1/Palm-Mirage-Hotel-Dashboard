9792a07af33d3067d4da1b99bc61c720c696fcc4
1OmarTarek1 <cr7omartarek@gmail.com>
2026-04-06 16:49:11 +0200

    refactor localhost usage for dev/prod environments

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,14 +1,19 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+import { loginSchemaType } from "@/schema/auth.schema";
+
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000"
+    : process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function loginUser(formData: loginSchemaType) {
-    const response = await fetch(`${BASE_URL}/auth/login`, {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
+  const response = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    const data = await response.json();
-    return data;
+  const data = await response.json();
+  return data;
 }
